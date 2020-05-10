8bea472430d29edf54a898ccf9a4e99fe8ff7f2c
John Visto <tapotsiv@gmail.com>
2020-05-10 13:34:16 +0800

    First commit from fork

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int magicBallNumber = 1;
+
+  void magicBallDecide () {
+    magicBallNumber = Random().nextInt(5) + 1;
+    print('I got clicked');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            magicBallDecide();
+          });
+        },
+        child: Image.asset('images/ball$magicBallNumber.png'),
+      ),
+    );
+  }
+}
